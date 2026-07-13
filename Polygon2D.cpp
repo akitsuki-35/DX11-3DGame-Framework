04385793d6bd65ec90dda2028283f15310083faf
あきつき <211625162+akitsuki -35@users.noreply.github.com>
2026-07-14 01:27:01 +0900

    rename: ファイルをパスカルケース化

---
+++ Polygon2D.cpp
@@ -0,0 +1,128 @@
+﻿/*============================================================
+*	@file	 : polygon2d.cpp
+*	@brief	 : 2Dポリゴン表示
+*
+* 　@author  : @akitsuki-35（https://github.com/akitsuki-35）
+* 　@date	 : 2026/04/28
+*	@updated : 2026/06/02
+*============================================================*/
+#include "main.h"
+#include "polygon2d.h"
+#include "renderer.h"
+
+Polygon2D::Polygon2D(const wchar_t* pFileName)
+{
+	// テクスチャ読込
+	TexMetadata metaData;
+	ScratchImage image;
+	LoadFromWICFile(pFileName, WIC_FLAGS_NONE, &metaData, image);
+	CreateShaderResourceView(Renderer::GetDevice(), image.GetImages(), image.GetImageCount(), metaData, &_mTexture);
+	assert(_mTexture);
+}
+
+void Polygon2D::Initialize(const XMFLOAT2& position, const XMFLOAT2& size, const WCHAR* fileName)
+{
+	mLayer = 3;
+
+	VERTEX_3D vertex[4];
+
+	vertex[0].Position = XMFLOAT3(position.x, position.y, 0.0f);
+	vertex[0].Normal = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	vertex[0].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[0].TexCoord = XMFLOAT2(0.0f, 0.0f);
+
+	vertex[1].Position = XMFLOAT3(position.x + size.x, position.y, 0.0f);
+	vertex[1].Normal = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	vertex[1].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[1].TexCoord = XMFLOAT2(1.0f, 0.0f);
+
+	vertex[2].Position = XMFLOAT3(position.x, position.y + size.y, 0.0f);
+	vertex[2].Normal = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	vertex[2].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[2].TexCoord = XMFLOAT2(0.0f, 1.0f);
+
+	vertex[3].Position = XMFLOAT3(position.x + size.x, position.y + size.y, 0.0f);
+	vertex[3].Normal = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	vertex[3].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[3].TexCoord = XMFLOAT2(1.0f, 1.0f);
+
+	// 頂点バッファ生成
+	D3D11_BUFFER_DESC bd{};
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(VERTEX_3D) * 4;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA sd{};
+	sd.pSysMem = vertex;
+
+	Renderer::GetDevice()->CreateBuffer(&bd, &sd, &_mVertexBuffer);
+
+	// テクスチャ読込
+	TexMetadata metaData;
+	ScratchImage image;
+	LoadFromWICFile(fileName, WIC_FLAGS_NONE, &metaData, image);
+	CreateShaderResourceView(Renderer::GetDevice(), image.GetImages(), image.GetImageCount(), metaData, &_mTexture);
+	assert(_mTexture);
+
+	// シェーダー読込
+	Renderer::CreateVertexShader(&_mVertexShader, &_mVertexLayout, "Resources\\Shaders\\unlitTextureVS.cso");
+	Renderer::CreatePixelShader(&_mPixelShader, "Resources\\Shaders\\unlitTexturePS.cso");
+}
+
+void Polygon2D::Finalize()
+{
+	if(_mTexture) _mTexture->Release();
+
+	_mPixelShader->Release();
+	_mVertexShader->Release();
+	_mVertexLayout->Release();
+	_mVertexBuffer->Release();
+}
+
+void Polygon2D::Update()
+{
+}
+
+void Polygon2D::Draw() const
+{
+	// 入力レイアウト設定
+	Renderer::GetDeviceContext()->IASetInputLayout(_mVertexLayout);
+
+	// シェーダー設定
+	Renderer::GetDeviceContext()->VSSetShader(_mVertexShader, NULL, 0);
+	Renderer::GetDeviceContext()->PSSetShader(_mPixelShader, NULL, 0);
+
+	// マトリクス設定
+	Renderer::SetWorldViewProjection2D();
+
+	XMMATRIX w, s, r, t;
+	s = XMMatrixScaling(1.0f, 1.0f, 1.0f); // 拡大縮小
+	r = XMMatrixRotationRollPitchYaw(0.0f, 0.0f, 0.0f); // 回転
+	t = XMMatrixTranslation(0.0f, 0.0f, 0.0f); // 平行移動
+	w = s * r * t;
+	Renderer::SetWorldMatrix(w);
+
+	// マテリアル設定
+	MATERIAL material{};
+	material.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	if(_mTexture)material.TextureEnable = true;
+	else material.TextureEnable = false;
+	Renderer::SetMaterial(material);
+
+	// テクスチャ設定
+	if (_mTexture) {
+		Renderer::GetDeviceContext()->PSSetShaderResources(0, 1, &_mTexture);
+	}
+
+	// 頂点バッファ設定
+	UINT stride = sizeof(VERTEX_3D);
+	UINT offset = 0;
+	Renderer::GetDeviceContext()->IASetVertexBuffers(0, 1, &_mVertexBuffer, &stride, &offset);
+
+	// プリミティブトポロジ設定
+	Renderer::GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+
+	// 描画
+	Renderer::GetDeviceContext()->Draw(4, 0);
+}
---
+++ Texture.h
@@ -0,0 +1,94 @@
+﻿/*============================================================
+*	@file	 : texture.h
+*	@brief	 : テクスチャ管理
+*
+* 　@author  : @akitsuki-35（https://github.com/akitsuki-35）
+* 　@date	 : 2026/04/13
+*	@updated : 2026/06/02
+*============================================================*/
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <d3d11.h>
+#include <string>
+#include <DirectXMath.h>
+
+/*============================================================
+*	@class	: Texture
+*	@brief	: テクスチャクラス
+*============================================================*/
+class Texture
+{
+protected:
+	DirectX::XMUINT2 originalSize{}; // テクスチャのオリジナルサイズ
+	ID3D11ShaderResourceView* pShaderResourceView{ nullptr }; // シェーダーリソースビュー
+
+	DirectX::XMFLOAT2 position{}; // 描画座標
+	DirectX::XMFLOAT2 drawSize{}; // 描画サイズ
+	float rotate{}; // 回転角
+	DirectX::XMFLOAT4 color{ 1.0f, 1.0f, 1.0f, 1.0f }; // カラー
+
+public:
+	Texture(const wchar_t* pFileName, const DirectX::XMFLOAT2& position = { 0.0f, 0.0f },
+		const DirectX::XMFLOAT2& size = { 0.0f, 0.0f }, const float& rotate = 0.0f,
+		const DirectX::XMFLOAT4& color = { 1.0f, 1.0f, 1.0f, 1.0f }, bool isMipMap = true);
+
+	virtual ~Texture();
+
+	// 描画
+	virtual void Draw();
+	virtual void SetTexture();
+
+	const DirectX::XMUINT2& GetSize() { return originalSize; }
+
+/*------------------------------------------------------------
+	セッター
+------------------------------------------------------------*/
+	void SetPosition(const DirectX::XMFLOAT2& newPosition) { position = newPosition; }
+	void SetSize(const DirectX::XMFLOAT2& newSize) { drawSize = newSize; }
+	void SetRotate(const float& newRotate) { rotate = newRotate; }
+	void SetColor(const DirectX::XMFLOAT4& newColor) { color = newColor; }
+
+	void BulkUpdate(const DirectX::XMFLOAT2& newPosition, const DirectX::XMFLOAT2& newSize,
+		const float& newRotate, const DirectX::XMFLOAT4& newColor) {
+		SetPosition(newPosition), SetSize(newSize), SetRotate(newRotate), SetColor(newColor);
+	}
+};
+
+/*============================================================
+*	@class	: SpriteSheet
+*	@brief	: スプライトシートクラス（テクスチャを継承）
+*============================================================*/
+class SpriteSheet : public Texture
+{
+private:
+	int patternMax{}; // パターン数
+	int CurrentPattern{}; // 現在のパターン番号
+	DirectX::XMUINT2 patternMatrix{}; // 縦・横のパターン数
+	DirectX::XMUINT2 patternSize{}; // パターンごとのサイズ
+
+public:
+	SpriteSheet(const wchar_t* pFileName, const DirectX::XMUINT2& patternMatrix,
+		const DirectX::XMFLOAT2& position = { 0.0f, 0.0f },
+		const DirectX::XMFLOAT2& size = { 0.0f, 0.0f }, const float& rotate = 0.0f,
+		const DirectX::XMFLOAT4& color = { 1.0f, 1.0f, 1.0f, 1.0f }, bool isMipMap = true);
+
+	// 描画
+	void Draw() override;
+
+/*------------------------------------------------------------
+	スプライト関係ゲッター・セッター
+------------------------------------------------------------*/
+	const int& GetCurrentPattern() { return CurrentPattern; }
+	const int& GetPatternMax() { return patternMax; }
+	const DirectX::XMUINT2& GetPatternMatrix() { return patternMatrix; }
+	const DirectX::XMUINT2& GetPatternSize() { return patternSize; }
+
+	void SetPattern(int patternNum) {
+		if (patternMax < patternNum) return;
+		CurrentPattern = patternNum;
+	}
+};
+
+#endif // TEXTURE_H
+
